5dd7fe74fc9f8fc3eabfd5c63414a6f864359791
wearrqqws <rowancole19@gmail.com>
2022-12-28 14:17:13 -0600

    smarter rand func

--- public/js/af/random.js
+++ public/js/af/random.js
@@ -2,7 +2,7 @@ let randomData;
 async function randomAnime() {
     let randomIcn = document.getElementById('random-btn').className = "fa-regular fa-spinner-third"
     document.getElementById('random-btn').classList.add("spinning")
-    let randomOutput = await fetch('http://localhost:8081/http://localhost:8080/meta/anilist/random-anime');
+    let randomOutput = await fetch('https://cors.haikei.xyz/https://api.haikei.xyz/meta/anilist/random-anime');
     randomData = await randomOutput.json()
     window.location.href = '/watch/' + randomData.title.romaji.replace(/ /g, '-').replace('.', '').replace(',', '').replace('/', '').replace('?', '').replace(/:/g, '').replace(/!/g, '').replace('∞', '').replace(/☆/g, '-').replace(/;/g, '').replace(/~/g, '').replace(/ä/g, 'a') + '-episode-1'
 }
