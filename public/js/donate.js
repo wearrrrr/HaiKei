d9111c64d87a21a43f1fd12b206d0e527f1eceba
wearrrrr <rowancole19@gmail.com>
2023-02-09 15:57:36 -0600

    Add toasts and click to copy

--- public/js/donate.js
+++ public/js/donate.js
@@ -1,5 +1,15 @@
+let loadedFlag = false;
 function copyAddr(addr) {
     navigator.clipboard.writeText(addr)
+    // dynamically load css only the first time
+    if (loadedFlag == false) {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css";
+        document.head.appendChild(link);
+        loadedFlag = true;
+    }
+    
     Toastify({
         text: "Copied to clipboard!",
         duration: 3000,
@@ -7,5 +17,7 @@ function copyAddr(addr) {
             background: "var(--dark-accent)",
             boxShadow: "none"
         },
+        close: true,
     }).showToast();
+
 }
